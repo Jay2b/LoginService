a1dbe6149af5737dc317d90aded7b93687c3ba65
jakubfieden <jakub.fieden@gmail.com>
2023-02-18 15:09:41 +0000

    Initial commit

---
+++ src/main/java/com/login/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.login.repository;
+
+import com.login.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
